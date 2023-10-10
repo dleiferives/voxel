99453cb62d9d8ee6745dcb146ad1385c1728c914
dleiferives <dleiferives@gmail.com>
2023-10-10 12:13:56 -0700

    main written

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Clean up and exit
+    glfwTerminate();
+    return 0;
+}
+
