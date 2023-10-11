b9a699a3c5a1ad7a98b54c58335f0934332a0e9b
dleiferives <dleiferives@gmail.com>
2023-10-11 00:17:51 -0700

    svo start

--- compute_shader.glsl
+++ compute_shader.glsl
@@ -8,33 +8,43 @@ uniform vec3 cameraPos;
 uniform vec3 cameraDir;
 uniform vec3 cameraUp;
 uniform vec3 cameraRight;
-uniform float cameraFov;  // Add this line for the FOV uniform
+uniform float cameraFov;
 
 // SVO data would also be passed in some form, either as a texture, SSBO, etc.
+
+float querySVO(vec3 position) {
+    // Query the SVO and return some value (e.g., density)
+    // This is a placeholder; you'll need to implement this based on how your SVO data is stored
+    return 0.0;
+}
 
 void main() {
     ivec2 storePos = ivec2(gl_GlobalInvocationID.xy);
     vec2 fragCoord = vec2(storePos) / vec2(640, 480); // Assuming a 640x480 resolution
 
-    // Incorporate FOV into the ray direction calculation
     float fovScale = tan(cameraFov * 0.5);
     vec3 rayDir = normalize(cameraDir +
                             (fragCoord.x - 0.5) * cameraRight * fovScale +
                             (fragCoord.y - 0.5) * cameraUp * fovScale);
 
-    // Initialize ray at camera position
     vec3 rayPos = cameraPos;
-
     vec4 pixel = vec4(0.0, 0.0, 0.0, 1.0); // Default to black
 
-    // Perform SVO traversal (simplified)
-    // while (not at end of ray) {
-    //     if (ray intersects SVO) {
-    //         pixel = color of intersected voxel;
-    //         break;
-    //     }
-    //     move ray forward;
-    // }
+    float stepSize = 0.1; // Size of each step along the ray
+    float t = 0.0; // Current distance along the ray
+
+    // Perform ray marching
+    for (int i = 0; i < 100; ++i) { // Limit the number of steps to avoid infinite loops
+        float density = querySVO(rayPos + rayDir * t);
+
+        if (density > 0.5) {
+            // We hit something!
+            pixel = vec4(1.0, 0.0, 0.0, 1.0); // Set to red for demonstration
+            break;
+        }
+
+        t += stepSize;
+    }
 
     imageStore(destTex, storePos, pixel);
 }
