dee69baba9f503d91909c8f6de7c865c48ed0d03
dleiferives <dleiferives@gmail.com>
2023-10-13 11:59:21 -0700

    working on implementing blocks

--- compute_shader.glsl
+++ compute_shader.glsl
@@ -4,32 +4,62 @@ layout (local_size_x = 16, local_size_y = 16) in;
 
 layout (rgba32f, binding = 0) writeonly uniform image2D destTex;
 
-layout (std430, binding = 1) buffer SVOData {
-    uint svo[]; // The actual SVO data
+
+struct Block{
+    uint voxels[64];
 };
 
-layout (std430, binding = 2) buffer IndirectionGrid {
-    uint grid[]; // Each element is an index into the SVOData buffer
-};
+//layout (std430, binding = 1) buffer blockPages {
+//    struct Block pages[]; // The actual SVO data
+//};
+
+
+//layout (std430, binding = 2) buffer IndirectionGrid {
+/    uint grid[]; // Each element is an index into the SVOData buffer
+//};
+
+
+
+int mortonCode(int x, int y, int z)
+{
+    int result =0;
+    result |=  ((z & 0x1) << 2) | ((y & 0x1 ) << 1)| ((x & 0x1)); // insert first order bits
+    result |=  ((z & 0x2) << 2) | ((y & 0x2) << 1)| ((x & 0x2)) << 3; // shift up the second order bits
+    result = (z | y | x) > 3 ? -result : result; // if z y x out of range, return negative
+    return result;
+}
+
+int mortonCode(vec3 pos)
+{
+    return mortonCode(pos.x, pos.y, pos.z);
+}
+
+// set gridsize
+const int gridSize = 64;
+
+int getBlockIndex(vec3 pos)
+{
+    int x = int(pos.x);
+    int y = int(pos.y);
+    int z = int(pos.z);
+    if(x == 1 && y == 1 && z == 1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+uint getBlockDensity(vec3 pos, int index)
+{
+    if (index ==0) return 0;
+    // find if it is in the block
+}
 
 uniform vec3 cameraPos;
 uniform vec3 cameraDir;
 uniform vec3 cameraUp;
 uniform vec3 cameraRight;
 uniform float cameraFov;
-
-uint querySVO(vec3 position, uint rootIndex) {
-    // Query the SVO starting at rootIndex and return some value
-    // This is a placeholder; you'll need to implement this based on how your SVO data is stored
-    return svo[rootIndex];
-}
-
-float calculateStepSize(vec3 rayPos, uint rootIndex) {
-    // Calculate the step size based on the current SVO or child SVO
-    // This is a placeholder; you'll need to implement this based on how your SVO data is stored
-    // For example, you might look up the size of the current node in the SVO and use that as the step size
-    return svo[rootIndex].size;
-}
 
 void main() {
     ivec2 storePos = ivec2(gl_GlobalInvocationID.xy);
@@ -48,8 +78,10 @@ void main() {
 
     // Perform ray marching
     for (int i = 0; i < 100; ++i) { // Limit the number of steps to avoid infinite loops
-        uint rootIndex = grid[int(rayPos.x) + int(rayPos.y) * gridSize + int(rayPos.z) * gridSize * gridSize];
-        uint density = querySVO(rayPos + rayDir * t, rootIndex);
+        //uint rootIndex = grid[int(rayPos.x) + int(rayPos.y) * gridSize + int(rayPos.z) * gridSize * gridSize];
+        //uint density = querySVO(rayPos + rayDir * t, rootIndex);
+        uint BlockIndex = getBlockIndex(rayPos);
+        uint density =
 
         if (density > 0) {
             // We hit something!
