08c35c4c5d452622db6c990b0f40c6e92301e409
dleiferives <dleiferives@gmail.com>
2024-04-16 15:13:04 -0700

    FPS and triangle

--- tut/2/main.c
+++ tut/2/main.c
@@ -7,6 +7,24 @@
     #define GS_GL_VERSION_STR "#version 430 core\n"
 #endif
 
+
+
+// FPS CAMERA //////////////////////////////////////////////////////////////////
+#define SENSITIVITY 0.1f
+#define CAM_SPEED 5.f
+
+typedef struct fps_camera_t {
+    float pitch;
+    float bob_time;
+    gs_camera_t cam;
+} fps_camera_t;
+
+fps_camera_t         fps = {0};
+void fps_camera_update(fps_camera_t* cam);
+
+#define rand_range(MIN, MAX)\
+    (rand() % (MAX - MIN + 1) + MIN)
+
 // DATA ////////////////////////////////////////////////////////////////////////
 
 float v_triangle[] ={
@@ -37,7 +55,7 @@ const char* vertex_shader =
 gs_command_buffer_t                    command_buffer = {0};
 gs_handle(gs_graphics_pipeline_t)            pipeline = {0};
 gs_handle(gs_graphics_shader_t)                shader = {0};
-gs_camera_t                                       cam = {0};
+//gs_camera_t                                       cam = {0};
 
 gs_handle(gs_graphics_vertex_buffer_t)   vbo_triangle = {0};
 gs_handle(gs_graphics_uniform_buffer_t)         ub_vp = {0};
@@ -61,10 +79,8 @@ void app_init(){
         command_buffer = gs_command_buffer_new();
 
         // Set up the camera
-        cam = gs_camera_perspective();
-        cam.transform.position = (gs_vec3){4.0,3.0,3.0};
-        cam.transform.scale = (gs_vec3){1.0,1.0,1.0};
-        cam.transform.rotation = (gs_vec3){0.0,1.0,0.0};
+        fps.cam = gs_camera_perspective();
+
         // Set up our vertex buffer 
         vbo_triangle = gs_graphics_vertex_buffer_create(
                 &(gs_graphics_vertex_buffer_desc_t) {
@@ -85,7 +101,7 @@ void app_init(){
         // set up uniform buffer for view projection
         gs_vec2 window_size = gs_platform_window_sizev(gs_platform_main_window());
         gs_mat4 projection = gs_camera_get_proj(
-                &cam,
+                &(fps.cam),
                 (uint32_t)window_size.x,
                 (uint32_t)window_size.y
         );
@@ -150,9 +166,21 @@ void app_update(){
         // clear the screen 
         gs_graphics_clear_desc_t clear = {.actions = &(gs_graphics_clear_action_t){.color = {0.1f, 0.1f, 0.1f, 1.f}}};
 
+
+        if (gs_platform_mouse_pressed(GS_MOUSE_LBUTTON) && !gs_platform_mouse_locked()) {
+                fps.cam.transform.rotation = gs_quat_default();
+                fps.pitch = 0.f;
+                gs_platform_lock_mouse(gs_platform_main_window(), true);
+        }
+
+        // Update camera
+        if (gs_platform_mouse_locked()) {
+                fps_camera_update(&fps);
+        }
+
         // Camera view
         // TODO: update with a fps camera
-        gs_mat4 view = gs_camera_get_view(&cam);
+        gs_mat4 view = gs_camera_get_view(&(fps.cam));
 
         // Request view update once per frame!
         gs_graphics_uniform_buffer_request_update(
@@ -209,4 +237,42 @@ gs_app_desc_t gs_main(int32_t argc, char** argv)
         .init = app_init,
         .update = app_update
     };
+}
+
+
+void fps_camera_update(fps_camera_t* fps)
+{
+    gs_platform_t* platform = gs_subsystem(platform);
+
+    gs_vec2 dp = gs_vec2_scale(gs_platform_mouse_deltav(), SENSITIVITY);
+    const float mod = gs_platform_key_down(GS_KEYCODE_LEFT_SHIFT) ? 2.f : 1.f;
+    float dt = platform->time.delta;
+    float old_pitch = fps->pitch;
+
+    // Keep track of previous amount to clamp the camera's orientation
+    fps->pitch = gs_clamp(old_pitch + dp.y, -90.f, 90.f);
+
+    // Rotate camera
+    gs_camera_offset_orientation(&fps->cam, -dp.x, old_pitch - fps->pitch);
+
+    gs_vec3 vel = {0};
+    if (gs_platform_key_down(GS_KEYCODE_W)) vel = gs_vec3_add(vel, gs_camera_forward(&fps->cam));
+    if (gs_platform_key_down(GS_KEYCODE_S)) vel = gs_vec3_add(vel, gs_camera_backward(&fps->cam));
+    if (gs_platform_key_down(GS_KEYCODE_A)) vel = gs_vec3_add(vel, gs_camera_left(&fps->cam));
+    if (gs_platform_key_down(GS_KEYCODE_D)) vel = gs_vec3_add(vel, gs_camera_right(&fps->cam));
+
+    // For a non-flying first person camera, need to lock the y movement velocity
+    vel.y = 0.f;
+
+    fps->cam.transform.position = gs_vec3_add(fps->cam.transform.position, gs_vec3_scale(gs_vec3_norm(vel), dt * CAM_SPEED * mod));
+
+    // If moved, then we'll "bob" the camera some
+    if (gs_vec3_len(vel) != 0.f) {
+        fps->bob_time += dt * 8.f;
+        float sb = sin(fps->bob_time);
+        float bob_amt = (sb * 0.5f + 0.5f) * 0.1f * mod;
+        float rot_amt = sb * 0.0004f * mod;
+        fps->cam.transform.position.y = 2.f + bob_amt;
+        fps->cam.transform.rotation = gs_quat_mul(fps->cam.transform.rotation, gs_quat_angle_axis(rot_amt, GS_ZAXIS));
+    }
 }
