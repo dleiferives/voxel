8cb4d150f87fa1579269caebe65c8b17430e9b2d
dleiferives <dleiferives@gmail.com>
2024-04-16 16:08:53 -0700

    failing color implementation

--- tut/3/main.c
+++ tut/3/main.c
@@ -71,17 +71,60 @@ float v_cube[] = {
     -1.0f, 1.0f, 1.0f,
     1.0f,-1.0f, 1.0f
 };
+
+float a_colors[] = {
+    0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f,
+    0.822f,  0.569f,  0.201f,
+    0.435f,  0.602f,  0.223f,
+    0.310f,  0.747f,  0.185f,
+    0.597f,  0.770f,  0.761f,
+    0.559f,  0.436f,  0.730f,
+    0.359f,  0.583f,  0.152f,
+    0.483f,  0.596f,  0.789f,
+    0.559f,  0.861f,  0.639f,
+    0.195f,  0.548f,  0.859f,
+    0.014f,  0.184f,  0.576f,
+    0.771f,  0.328f,  0.970f,
+    0.406f,  0.615f,  0.116f,
+    0.676f,  0.977f,  0.133f,
+    0.971f,  0.572f,  0.833f,
+    0.140f,  0.616f,  0.489f,
+    0.997f,  0.513f,  0.064f,
+    0.945f,  0.719f,  0.592f,
+    0.543f,  0.021f,  0.978f,
+    0.279f,  0.317f,  0.505f,
+    0.167f,  0.620f,  0.077f,
+    0.347f,  0.857f,  0.137f,
+    0.055f,  0.953f,  0.042f,
+    0.714f,  0.505f,  0.345f,
+    0.783f,  0.290f,  0.734f,
+    0.722f,  0.645f,  0.174f,
+    0.302f,  0.455f,  0.848f,
+    0.225f,  0.587f,  0.040f,
+    0.517f,  0.713f,  0.338f,
+    0.053f,  0.959f,  0.120f,
+    0.393f,  0.621f,  0.362f,
+    0.673f,  0.211f,  0.457f,
+    0.820f,  0.883f,  0.371f,
+    0.982f,  0.099f,  0.879f
+};
+
 const char* fragment_shader = 
         GS_GL_VERSION_STR 
         "precision mediump float;\n"
+        "in vec3 f_color;\n"
         "out vec3 color;\n"
         "void main(){\n"
-        "       color = vec3(1,0,0);\n"
+        "       color = f_color;\n"
         "}\n";
 
 const char* vertex_shader = 
         GS_GL_VERSION_STR
         "layout(location = 0) in vec3 a_pos;\n"
+        "layout(location = 1) in vec3 a_color;\n"
+        "out vec3 f_color;\n"
         "layout (std140) uniform ub_vp {\n"
         "   mat4 projection;\n"
         "   mat4 view;\n"
@@ -95,7 +138,8 @@ gs_handle(gs_graphics_pipeline_t)            pipeline = {0};
 gs_handle(gs_graphics_shader_t)                shader = {0};
 //gs_camera_t                                       cam = {0};
 
-gs_handle(gs_graphics_vertex_buffer_t)   vbo_triangle = {0};
+gs_handle(gs_graphics_vertex_buffer_t)   vbo_cube = {0};
+gs_handle(gs_graphics_vertex_buffer_t)   vbo_color= {0};
 gs_handle(gs_graphics_uniform_buffer_t)         ub_vp = {0};
 
 typedef struct v_viewproj_t{
@@ -120,10 +164,18 @@ void app_init(){
         fps.cam = gs_camera_perspective();
 
         // Set up our vertex buffer 
-        vbo_triangle = gs_graphics_vertex_buffer_create(
+        vbo_cube= gs_graphics_vertex_buffer_create(
                 &(gs_graphics_vertex_buffer_desc_t) {
                         .data = v_cube,
                         .size = sizeof(v_cube)
+                }
+        );
+        //
+        // Set up our vertex buffer 
+        vbo_color = gs_graphics_vertex_buffer_create(
+                &(gs_graphics_vertex_buffer_desc_t) {
+                        .data = a_colors,
+                        .size = sizeof(a_colors)
                 }
         );
 
@@ -183,7 +235,8 @@ void app_init(){
                         },
                         .layout = {
                                 .attrs = (gs_graphics_vertex_attribute_desc_t[]) {
-                                        {.format = GS_GRAPHICS_VERTEX_ATTRIBUTE_FLOAT3, .name = "a_pos"}
+                                        {.format = GS_GRAPHICS_VERTEX_ATTRIBUTE_FLOAT3, .name = "a_pos"},
+                                        {.format =  GS_GRAPHICS_VERTEX_ATTRIBUTE_FLOAT3, .name = "a_color", .buffer_idx = 1}
                                 },
                                 .size = sizeof(gs_graphics_vertex_attribute_desc_t)
                         }
@@ -235,17 +288,20 @@ void app_update(){
         );
 
 
+        gs_graphics_bind_vertex_buffer_desc_t v_buffers[2] = {
+                (gs_graphics_bind_vertex_buffer_desc_t){.buffer = vbo_cube},
+                (gs_graphics_bind_vertex_buffer_desc_t){.buffer = vbo_color}
+        };
         // Render //
         gs_graphics_renderpass_begin(&command_buffer, GS_GRAPHICS_RENDER_PASS_DEFAULT);
                 gs_graphics_set_viewport(&command_buffer, 0,0, (uint32_t)fs.x, (uint32_t)fs.y );
                 // using the clear action
                 gs_graphics_clear(&command_buffer, &clear);
-                // buffer binds though there is only the vbo_triangle for now
+
                 gs_graphics_bind_desc_t binds = {
                         .vertex_buffers = {
-                                &(gs_graphics_bind_vertex_buffer_desc_t){
-                                        .buffer = vbo_triangle
-                                }
+                               .desc = v_buffers ,
+                                .size = 2
                         },
                         .uniform_buffers = {
                                 &(gs_graphics_bind_uniform_buffer_desc_t){
